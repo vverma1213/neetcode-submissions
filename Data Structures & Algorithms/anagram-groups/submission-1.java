0e582bb1bdcd54cdad2387cb572b6be8810f499c
Vivek Verma <vivek.verma012@outlook.com>
2026-08-31 18:40:53 +0530

    Add: anagram-groups - submission-1

---
+++ Data Structures & Algorithms/anagram-groups/submission-1.java
@@ -0,0 +1,18 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        //Approach 1 - Iterate each string sort and group
+        Map<String,List<String>> map = new HashMap<>();
+        
+        for(String str:strs){
+            char[] chr = str.toCharArray();
+            Arrays.sort(chr);
+            // if(map.containsKey(new String(chr))){
+            //     map.put(new String(chr),map.get(new String(chr))).add(str);
+            // } else{
+            //     map.put(new String(chr),new ArrayList<>(Arrays.asList(str)));
+            // }
+            map.computeIfAbsent(new String(chr), k->new ArrayList<>()).add(str);
+        }
+        return new ArrayList<>(map.values());
+    }
+}
