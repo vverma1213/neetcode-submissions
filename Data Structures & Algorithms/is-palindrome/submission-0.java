e536fe52bc49fb03f5cdb31153582582e2fcd966
Vivek Verma <vivek.verma012@outlook.com>
2026-09-15 21:54:28 +0530

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        s = s.replaceAll("[^a-zA-Z0-9]","").toLowerCase();
+        int left =0;
+        int right = s.length()-1;
+
+        while(left<right){
+            if(s.charAt(right)!=s.charAt(left))
+            {
+                return false;
+            }
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+}
